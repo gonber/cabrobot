05abb913f288538a9b3d56aef0f14d7aa7646b79
Goncalo Bernardo <gb@Goncalos-MacBook-Pro.local>
2016-02-28 16:37:36 +0800

    raw python

---
+++ app.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import time
+import pprint
+import telepot
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+def handle(msg):
+    pprint.pprint(msg)
+    # Do your stuff here ...
+
+
+# Getting the token from command-line is better than embedding it in code,
+# because tokens are supposed to be kept secret.
+TOKEN = os.environ.get('TELEGRAM_API_TOKEN') # sys.argv[1]
+bot = telepot.Bot(TOKEN)
+bot.notifyOnMessage(handle)
+print 'Listening ...'
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
