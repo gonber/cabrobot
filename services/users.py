6cf0e7260c403296827fe048e4fcf967c60a8d98
Goncalo Bernardo <gb@Goncalos-MacBook-Pro.local>
2016-03-09 10:08:28 +0800

    lastModified issues

--- services/users.py
+++ services/users.py
@@ -1,26 +1,30 @@
 import pymongo
 import os
-from datetime import datetime
+from datetime import datetime, date
 
 
 class Users():
-  def __init__(self):
-    url = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017/testdb')
-    self._client = pymongo.MongoClient(url)
-    self._db = self._client.get_default_database()
-    self._collection = self._db['users']
-    self._collection.update_one({'_id': -1}, # bootstrap if needed
-      {'$set': {'lastModified': datetime.utcnow()}},upsert=True)
+    def __init__(self):
+        url = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017/testdb')
+        self._client = pymongo.MongoClient(url)
+        self._db = self._client.get_default_database()
+        self._collection = self._db['users']
+        self._collection.update_one({'_id': -1}, # bootstrap if needed
+          {'$set': {'lastModified': datetime.utcnow()}}, upsert=True)
 
-  def get_user(self, user_id):
-    self._collection.update_one({'_id': user_id},
-      {'$set': {'lastModified': datetime.utcnow()}},
-      upsert=True)
+    def get_user(self, user_id):
+        user = self._collection.find_one({'_id': user_id})
+        if user:
+            return user
 
-    return self._collection.find_one({'_id': user_id})
+        self._collection.update_one({'_id': user_id},
+            {'$set': {'lastModified': datetime(1989, 8, 24)}},
+            upsert=True)
 
-  def update_user(self, user):
-    user['lastModified'] = datetime.utcnow()
-    user_id = user.pop('_id')
-    self._collection.update_one({'_id': user_id},
-      {'$set': user})#, '$currentDate': {'lastModified': True}})
+        return self._collection.find_one({'_id': user_id})
+
+    def update_user(self, user):
+        user['lastModified'] = datetime.utcnow()
+        user_id = user.pop('_id')
+        self._collection.update_one({'_id': user_id},
+          {'$set': user})#, '$currentDate': {'lastModified': True}})
